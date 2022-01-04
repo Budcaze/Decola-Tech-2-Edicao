f2f49c733fdb8d06d0a7afe16aaeddb94b06410b
Eduardo Henrique Cazé de Almeida <66096920+Budcaze@users.noreply.github.com>
2022-01-04 20:26:37 -0300

    commit

---
+++ TreinoCS/EstudosExternosDIO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosExternosDIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
