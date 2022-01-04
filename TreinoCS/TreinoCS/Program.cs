67a239636298a12de669bd776b65d5a1c66323fc
Eduardo Henrique Cazé de Almeida <66096920+Budcaze@users.noreply.github.com>
2022-01-04 16:08:54 -0300

    commit

--- TreinoCS/TreinoCS/Program.cs
+++ TreinoCS/TreinoCS/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Decola_Tech
+namespace TreinoCS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
